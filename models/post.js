1067040607c3530a5f0ec7552c646a7fe7444b63
devopsdeepaks <deepaksinghalgpg@gmail.com>
2025-07-14 01:43:49 +0530

    f

--- models/post.js
+++ models/post.js
@@ -1,3 +1,9 @@
+const mongoose = require("mongoose");
+// mongoose.connect(process.env.MONGO_URI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+
 const postSchema = mongoose.Schema({
   user: {
     type: mongoose.Schema.Types.ObjectId,
