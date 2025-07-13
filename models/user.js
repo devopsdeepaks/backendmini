e5b679b807f7164afb8402db93320f8e86e3678e
devopsdeepaks <deepaksinghalgpg@gmail.com>
2025-07-14 01:26:14 +0530

    de

--- models/user.js
+++ models/user.js
@@ -2,10 +2,6 @@ const mongoose = require("mongoose");
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  ssl: true,
-  tlsInsecure: false, // force TLS
-  tlsAllowInvalidCertificates: false,
-  tlsAllowInvalidHostnames: false,
 });
 
 const userSchema = mongoose.Schema({
